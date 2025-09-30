1c526ba10ca9e6b0600937fd4fa64d730b517577
Ivan <ivan.georgiev2002k@gmail.com>
2025-09-30 16:59:32 +0300

    Setting up the server in the backend, creating a get route to test it.

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+const express = require("express");
+//express app
+const app = express();
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+//routes
+app.get("/", (req, res) => {
+  res.json({ msg: "Welcome to the app" });
+});
+
+//listen for requests
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
+});
